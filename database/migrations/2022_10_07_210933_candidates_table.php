ea8c6a037aded2f85b04f2f7c26ff1c2ad6e8a40
Ariel Di Salvo <projects@medular.me>
2022-10-16 12:08:26 +0200

    Migrations and seeders.

--- database/migrations/2022_10_07_210933_candidates_table.php
+++ database/migrations/2022_10_07_210933_candidates_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 45);
+            $table->string('last_name', 45);
+            $table->string('phone', 45);
+            $table->string('email')->unique();;
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::drop('candidates');
     }
 };
--- database/seeders/Candidates.php
+++ database/seeders/Candidates.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Candidate;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class Candidates extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            Candidates::class,
-        ]);
+        Candidate::factory(5)->create();
     }
 }
