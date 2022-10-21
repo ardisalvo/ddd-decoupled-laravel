6c046289bb392d61459e579a4eb0daf1cff7c9b5
Ariel Di Salvo <projects@medular.me>
2022-10-21 21:02:35 +0200

    Migration and model Offers.

---
+++ database/migrations/2022_10_07_210956_offers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title', 120);
+            $table->text('description');
+            $table->foreignUuid('company_id');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('offers');
+    }
+};
